a2e6f5d9edb6ae6de29e6a391513f102a355c5b8
Edward <130746635+Plodder123@users.noreply.github.com>
2023-06-06 12:13:55 +0100

    updated validation on timer model, added index on timer view, and changed timer controller name

--- app/models/timer.rb
+++ app/models/timer.rb
@@ -1,3 +1,5 @@
 class Timer < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
+  validates :ingredient, :duration, presence: true
+  validates :duration, numericality: { only_integer: true, greater_than: 0 }
 end
