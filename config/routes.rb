d2460a5c326ecd6a745c5644ac34a3ae1f2ad53a
Edward <130746635+Plodder123@users.noreply.github.com>
2023-06-06 15:24:32 +0100

    added timers controllers methods

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :recipes, only: :index
+  resources :timers, only: :index
   # Defines the root path route ("/")
   # root "articles#index"
 end
