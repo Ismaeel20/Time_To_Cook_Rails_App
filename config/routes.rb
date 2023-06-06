3c0d63ccb8d0b9f73cae2aab9758e3978f30eaa9
Edward <130746635+Plodder123@users.noreply.github.com>
2023-06-06 17:19:23 +0100

    added to the index, and added the search bar functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :recipes, only: :index
-  resources :timers, only: :index
+  resources :timers, only: [:index, :destroy]
   # Defines the root path route ("/")
   # root "articles#index"
 end
