bafed3583360683c6a228c2ad034d809f1f6ad6e
Warsame Jama <war.s@hotmail.co.uk>
2023-06-06 12:28:22 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts "Cleaning database"
+Timer.destroy_all
+
+# Timer.create(ingredient: 'pizza', time: 10)
+# Timer.create(ingredient: 'flour', time: 15)
+
+# user_one = User.create(email: "lewagon@lewagon.com", password: "123456")
+
+
+puts "creating timers"
+
+
+50.times do
+  Timer.create(ingredient: Faker::Food.unique.ingredient, time: rand(5..30))
+end
