efa77eaf33949fc30e186dc8ec4b8b0a8161d606
Edward <130746635+Plodder123@users.noreply.github.com>
2023-06-06 12:44:12 +0100

    fixed timers models, changed duration to time

--- app/models/recipe_timer.rb
+++ app/models/recipe_timer.rb
@@ -1,7 +1,7 @@
 class RecipeTimer < ApplicationRecord
   belongs_to :recipe, dependent: :destroy
 
-  validates :ingredient, :duration, presence: true
-  validates :duration, numericality: { only_integer: true, greater_than: 0 }
+  validates :ingredient, :time, presence: true
+  validates :time, numericality: { greater_than: 0 }
   validates :recipe, presence: true
 end
