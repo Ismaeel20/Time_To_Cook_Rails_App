2aeaff5b04ed1889c4bd30c43b585611cdd1ca27
Edward <130746635+Plodder123@users.noreply.github.com>
2023-06-07 10:58:23 +0100

    added logo

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :recipes, only: [:show, :create, :destroy, :index]
   resources :timers, only: [:index, :destroy]
->
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
