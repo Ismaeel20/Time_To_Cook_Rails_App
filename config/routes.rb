bfabda81a8233672ff6c49e5925f51c9ab44c5e0
Edward <130746635+Plodder123@users.noreply.github.com>
2023-06-07 14:31:09 +0100

    added edit route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :recipes, only: [:show, :create, :destroy, :index] do
+  resources :recipes, only: [:show, :create, :destroy, :index, :edit] do
     resources :recipe_timers, only: [:create]
   end
   resources :timers, only: [:index, :destroy]
